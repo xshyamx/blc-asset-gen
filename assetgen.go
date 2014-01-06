f1681b6189441dc846212f22ecf4dc6d4caad99f
shyam <xshyamx@gmail.com>
2014-01-07 00:01:20 +0530

    accepting command line arguments using flag package

--- assetgen.go
+++ assetgen.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"strings"
 	"crypto/md5"
+	"flag"
 )
  	
 func check(e error) {
@@ -27,20 +28,29 @@ func copy(src, dest string) bool {
 }
 
 func main() {
-	rootDir := "E:/workspaces/activ-wsp/broadleaf/core/src/main/resources/cms/static/img"
-	outDir  := "E:/workspaces/activ-wsp/broadleaf/site/target/assets"
+	rootDir := flag.String("src", "", "Source folder to copy assets")
+	outDir  := flag.String("dest", "target", "Target folder to copy assets")
+	flag.Parse()
+	if *rootDir == "" {
+		flag.PrintDefaults()
+		return
+	}
 	fileCount := 0
-	filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+	filepath.Walk(*rootDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		url := strings.Replace(path, "\\", "/", -1)
-		url = strings.Replace(url, rootDir, "/img", 1)
+		url = strings.Replace(url, *rootDir, "/img", 1)
+		filename := filepath.Base(path)
 		if !info.IsDir() {
+			if filename[0] == '.' && len(filename) > 2 {
+				return filepath.SkipDir
+			}
 			md5 := md5.New()
 			md5.Write([]byte(url))
 			digest := fmt.Sprintf("%x", md5.Sum(nil))
-			dest := filepath.Join(outDir, digest[0:2], digest[2:4], filepath.Base(path))
+			dest := filepath.Join(*outDir, digest[0:2], digest[2:4], filename)
 			if copy(path, dest) {
 				fileCount++
 			}
